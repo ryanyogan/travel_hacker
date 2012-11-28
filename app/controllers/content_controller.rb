0c2037be41c97508d5dd833e02db3c7bd303dd0e
Ryan Yogan <ryanyogan@gmail.com>
2012-11-28 00:35:38 -0600

    add authorization

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,7 +1,11 @@
 class ContentController < ApplicationController
+  before_filter :authenticate_user!
+
   def standard
+    authorize! :view, :standard, :message => 'Access limited to Standard subscribers.'
   end
 
   def plus
+    authorize! :view, :plus, :message => 'Access limited to Plus Subscribers.'
   end
 end
