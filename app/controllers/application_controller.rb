b526263a1e476ee2176334dceefeb76e45722aeb
Ryan Yogan <ryanyogan@gmail.com>
2012-11-28 00:51:46 -0600

    redirect after log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,16 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, :alert => exception.message
   end
 
+  def after_sign_in_path_for(resource)
+    case current_user.roles.first.name
+    when 'admin'
+      users_path
+    when 'standard'
+      content_standard_path
+    when 'plus'
+      content_plus_path
+    else
+      root_path
+    end
+  end
 end
