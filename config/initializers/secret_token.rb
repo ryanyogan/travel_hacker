7606d599dfb33d361871e09cfc8cf5acbab7eaee
Ryan Yogan <ryanyogan@gmail.com>
2012-11-27 23:40:20 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelHacker::Application.config.secret_token = '8f212202d1dc24e5521646c909f6b7d1965908785aeb26920580b4162ea5754ed9f92e7789ff67bab7ff55cbdc5ec7d2bde618f19e83bcb820066c5f25b7bfa3'
